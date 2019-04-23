039be24dfc8ede8fb2b09767b76cdad4176beb79
Perry Brandiezs <pbrandiezs@gmail.com>
2019-04-23 10:46:42 -0700

    feat: Added starter code for models.py

--- models.py
+++ models.py
@@ -1,28 +1,34 @@
-#!/usr/bin/env python
-
-from sqlalchemy import Column, Integer, String
+# -*- coding: utf-8 -*-
+from sqlalchemy import Column,Integer,String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 
+
+
+
+
 Base = declarative_base()
+class Restaurant(Base):
+  __tablename__ = 'restaurant'
+  id = Column(Integer, primary_key = True)
+  restaurant_name = Column(String)
+  restaurant_address = Column(String)
+  restaurant_image = Column(String)
+  
+  
+  #Add a property decorator to serialize information from this database
+  @property
+  def serialize(self):
+    return {
+      'restaurant_name': self.restaurant_name,
+      'restaurant_address': self.restaurant_address,
+      'restaurant_image' : self.restaurant_image,
+      'id' : self.id
+      
+      }
 
-class Puppy(Base):
-    __tablename__ = 'puppy'
+engine = create_engine('sqlite:///restaurants.db')
+ 
 
-
-    name =Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
-    description = Column(String(250))
-    #Add add a decorator property to serialize data from the database
-    @property
-    def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         : self.name,
-           'id'           : self.id,
-           'description'  : self.description
-       }
-
-
-engine = create_engine('sqlite:///puppies.db')
 Base.metadata.create_all(engine)
